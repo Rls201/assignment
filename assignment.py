e2b6fa276d7b58f724d85541daa5ed2d842b5bed
Rls201 <r.simms201@gmail.com>
2023-05-22 04:00:57 +0100

    sort by distance

--- assignment.py
+++ assignment.py
@@ -3,6 +3,8 @@ import geopandas as gpd
 import folium
 from geopy.geocoders import Nominatim
 from shapely import Point
+import fiona
+import shapely.geometry
 
 # calculate lat & lon from inputted location
 loc = Nominatim(user_agent="GetLoc")
@@ -15,11 +17,14 @@ print("Longitude = ", getLoc.longitude)
 
 m = folium.Map(location=(getLoc.latitude, getLoc.longitude), zoom_start=7, tiles="cartodb positron")
 
-pt = pd.DataFrame({
+pt = gpd.GeoDataFrame({
     'lat': [getLoc.latitude],
     'lon': [getLoc.longitude],
     'name': [getLoc.address],
-}, dtype=str)
+},
+    geometry=gpd.points_from_xy([getLoc.longitude], [getLoc.latitude]),  # add geometry to user location
+    crs='epsg:27700',  # 4326
+    dtype=str)
 
 print(pt)
 
@@ -37,11 +42,19 @@ for i in range(0, len(pt)):
 df = pd.read_csv('data_files/Airports.csv')  # read the csv data
 
 # create a new geodataframe
-airports = gpd.GeoDataFrame(df[['name', 'category']],  # use the csv data, but only the name/website columns
+airports = gpd.GeoDataFrame(df[['name', 'category', 'lon', 'lat']],
+                            # use the csv data, but only the name/website columns
                             geometry=gpd.points_from_xy(df['lon'], df['lat']),  # set the geometry using points_from_xy
-                            crs='epsg:4326')  # set the CRS using a text representation of the EPSG code for WGS84 lat/lon
+                            crs='epsg:27700',
+                            # set the CRS using a text representation of the EPSG code for WGS84 lat/lon
+                            dtype=str)
 
-airports.head()  # show the new geodataframe
+airports.set_crs(epsg=27700, inplace=True)
+
+airports['distance'] = airports['geometry'].distance(pt)
+airports.sort_values( by='distance', ascending=True, inplace=True)
+
+print(airports.head(5))  # show the new geodataframe
 
 # add the airport points to the existing map
 airports.explore('category',
